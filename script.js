4d4814da71cc250370d3d0e62d6d6aba4fefe02a
subsanddubs11 <subsanddubs11@tutanota.com>
2024-08-11 19:21:48 -0700

    Finished javascript logic

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const winnerMessage = document.getElementById('winner-message');
+const gameBoard = document.getElementById('game-board');
+
 function GameGrid () {
   const rows = 3;
   const cols = 3;
@@ -29,28 +32,31 @@ function GameGrid () {
 
   const checkSurroundingValues = (row, col) => {
     const directions  = [
-      {row: -1, col: 0}, // Square above
-      {row: 1, col: 0}, // Square below
-      {row: 0, col: -1}, // Square to the left
-      {row: 0, col: 1}, // Square to the right
-      {row: -1, col: -1}, // Square to the top-left diagonal
-      {row: -1, col: 1}, // Square to the top-right diagonal
-      {row: 1, col: -1}, // Square to the bottom-left diagonal
-      {row: 1, col: 1} // Square to the bottom-right diagonal
+      {name: 'above', orientation: 'vertical', row: -1, col: 0}, // Square above
+      {name: 'below', orientation: 'vertical', row: 1, col: 0}, // Square below
+      {name: 'left', orientation: 'horizontal', row: 0, col: -1}, // Square to the left
+      {name: 'right', orientation: 'horizontal', row: 0, col: 1}, // Square to the right
+      {name: 'top-left', orientation: 'downwards-diagonal', row: -1, col: -1}, // Square to the top-left diagonal
+      {name: 'top-right', orientation: 'upwards-diagonal', row: -1, col: 1}, // Square to the top-right diagonal
+      {name: 'bottom-left', orientation: 'upwards-diagonal', row: 1, col: -1}, // Square to the bottom-left diagonal
+      {name: 'bottom-right', orientation: 'downwards-diagonal', row: 1, col: 1} // Square to the bottom-right diagonal
     ]
 
     const values = {
-      mark: grid[row][col].getMark()
+      mark: grid[row][col].getMark(),
+      directions: {}
     };
 
-    for (const {row: r, col: c} of directions) {
-      const newRow = row + r;
-      const newCol = col + c;
+    for (const direction of directions) {
+      const { name, orientation, row: r, col: c } = direction;
+      let newRow = row + r;
+      let newCol = col + c;
+      values.directions[name] = [];
 
-      if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols) {
-        values[`(${newRow}, ${newCol})`] = grid[newRow][newCol].getMark();
-      } else {
-        values[`(${newRow}, ${newCol})`] = null;
+      while (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols) {
+        values.directions[name].push({orientationClass: orientation, value: grid[newRow][newCol].getMark()});
+        newRow += r;
+        newCol += c;
       }
     }
     
@@ -102,6 +108,34 @@ function GameController(
     console.log(`${getActivePlayer().name}'s turn`);
   };
 
+  const checkWin = (row, col) => {
+    const gridObject = grid.checkSurroundingValues(row, col);
+    const currentMark = gridObject.mark;
+    const directions = gridObject.directions;
+    const matchingSquares = [];
+
+    for (const direction in directions) {
+      const subArray = directions[direction];
+      for (let i = 0; i < 2; i++) {
+        if(!subArray[i]) {
+          continue;
+        } else if (subArray[i].value === currentMark) {
+          matchingSquares.push(subArray[i]);
+        }
+      }  
+    } 
+
+    for (let i = 0; i < matchingSquares.length; i++) {
+      for (let j = i + 1; j < matchingSquares.length; j++) {
+        if(matchingSquares[i].orientationClass === matchingSquares[j].orientationClass) {
+          return true;
+        }
+      }
+    }
+    
+    return false;
+  }
+
   const playRound = (row, col) => {
     let isValidMove = false;
 
@@ -112,20 +146,12 @@ function GameController(
     if(!isValidMove) {
       console.log('Invalid move. Try again')
     } else {
-      const valuesGrid = grid.checkSurroundingValues(row, col);
-      const currentMark = valuesGrid.mark;
-      const {mark, ...surroundingValues} = valuesGrid;
-      const surroundingValuesArray = Object.values(surroundingValues).filter(mark => mark !== null);
-      console.log('Values around the move: ', surroundingValuesArray);
-
-      const isMatching = surroundingValuesArray.some(mark => mark === currentMark);
-
-      if (isMatching) {
-        console.log('One of the surrounding values matches');
-      } else {
-        console.log('None of the surrounding values match');
-      }
-
+      if (checkWin(row, col) === true) {
+        winnerMessage.innerText = `${getActivePlayer().name} won the game!`;
+        grid.printGrid();
+        return;
+      } 
+    
       switchTurns();
       printNextTurn();
     }
@@ -137,3 +163,7 @@ function GameController(
 }
 
 const game = GameController();
+
+const p = (row, col) => {
+  game.playRound(row,col);
+}
